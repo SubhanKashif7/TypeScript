ee25d43882867f8156ac53c13092788b9d23772f
= <=>
2024-03-03 12:26:48 +0500

    n

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-var apiReq = function (url, headers) {
-    var res = fetch(url, headers).then(function (response) {
-        return response.json;
-    });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var total_marks = 100;
+var got_marks = 80;
+var percentageCalculator = function (total, got) {
+    if (total === void 0) { total = 100; }
+    if (got === void 0) { got = 0; }
+    var per = got_marks / total_marks * 100;
+    return per;
 };
+console.log(percentageCalculator(total_marks, got_marks));
+var TypeLang = /** @class */ (function () {
+    function TypeLang(parameters) {
+        console.log(parameters);
+    }
+    return TypeLang;
+}());
+;
+var obj = new TypeLang("Subhan");
--- src/tempCodeRunnerFile.ts
+++ src/tempCodeRunnerFile.ts
@@ -1,13 +1,10 @@
-const total_marks: number = 764;
+
 const got_marks: number = 80;
 
 const percentageCalculator = (total: number = 100,got: number = 0) : number => {
     let per: number = got_marks/total_marks*100;
-    return parseInt(per.toFixed(2))
+    return per
 };  
 
 
 console.log(percentageCalculator(total_marks,got_marks));
-
-export {}
-
